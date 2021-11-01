e4c08bdaa00d38770f1de8ec978d417464a0e0ec
Sheelove Dol <siesel26@gmail.com>
2021-11-01 06:42:16 -0700

    Add example for .onclick

--- main.js
+++ main.js
@@ -9,3 +9,40 @@ function showInfo() {
 
 // Adding event listener to event target to make readMore button interactive
 readMore.addEventListener('click', showInfo);
+
+
+
+// Example of using the .onclick method 
+
+/* 
+
+let view = document.getElementById('view-button');
+let close = document.getElementById('close-button');
+let codey = document.getElementById('codey');
+
+let open = function() {
+  codey.style.display = 'block';
+  close.style.display = 'block';
+};
+
+let hide = function() {
+  codey.style.display = 'none';
+  close.style.display = 'none';
+};
+
+view.addEventListener('click', open);
+close.addEventListener('click', hide);
+
+// Write your code here
+
+let textChange = function() {
+  view.innerHTML = 'Hello, World!';
+}
+
+let textReturn = function() {
+  view.innerHTML = 'View';
+}
+
+view.onclick = textChange;
+close.onclick = textReturn;
+*/
